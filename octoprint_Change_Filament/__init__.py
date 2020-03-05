1eb29ff96e2dcc86014c4efa09dcb776919e8839
jim-p <jimp@pfsense.org>
2020-03-04 22:53:06 -0500

    OctoPrint 1.4.0/Python 3.x compatibility. Fixes #18

--- octoprint_Change_Filament/__init__.py
+++ octoprint_Change_Filament/__init__.py
@@ -79,6 +79,7 @@ class Change_filamentPlugin(octoprint.plugin.SettingsPlugin,
 
 
 __plugin_name__ = "Change Filament Plugin"
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
 	global __plugin_implementation__
